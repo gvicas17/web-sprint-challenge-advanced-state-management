2d25d0877c203847ab8e57999ce94716f8d43ef6
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-11-06 11:39:28 -0600

    added dependiences and reducer

---
+++ smurfs/src/reducer/reducer.js
@@ -0,0 +1,13 @@
+
+export const initialState = {
+    smurfs: [],
+    error: ''
+}
+
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
