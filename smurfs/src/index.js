b77c8de2564ffdc7497f3d731ecda4bf0323baea
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-11-06 10:50:43 -0600

    added dependencies and created new folders/files

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render
+(<Provider store = {store}>
+    <App />
+</Provider>, document.getElementById("root"));
